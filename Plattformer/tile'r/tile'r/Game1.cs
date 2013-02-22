3b777148c22d4a3fb1c2c1a7bf1c0edac5e7bc4d
Marin Jelica <Xe3d@live.se>
2013-02-22 18:26:40 +0100

    Initial comitt
    the first one, share all the filezz

---
+++ Plattformer/tile'r/tile'r/Game1.cs
@@ -0,0 +1,304 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using TestGame.TileEngine;
+using Shooter;
+
+namespace tile_r
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Tile> tileList = new List<Tile>();
+        Texture2D crate;
+        Player player;
+        Texture2D sprite;
+        Vector2 playerVelocity = new Vector2(0, 0);
+        bool standing = false;
+        Rectangle playerRect;
+        float rotation;
+        KeyboardState prevKey;
+        bool test;
+
+        int[,] map = new int[,]
+            {
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,}
+
+
+            };
+
+
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            player = new Player();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            crate = Content.Load<Texture2D>("crate");
+
+            Createflor();
+
+            Animation animation = new Animation();
+
+            sprite = Content.Load<Texture2D>("jasperrun");
+            animation.Initialize(sprite, new Vector2(0, 0), 24, 42, 4, 100, Color.White, 1f, true);
+
+            player.Initialize(animation, new Vector2(50, 100));
+
+            KeyboardState prevKey = Keyboard.GetState();
+            
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+        private void Createflor()
+        {
+
+            int tileMapWidth = map.GetLength(1);
+            int tileMapHeight = map.GetLength(0);
+
+            for (int y = 0; y < tileMapHeight; y++)
+            {
+
+                for (int x = 0; x < tileMapWidth; x++)
+                {
+                    int textureIndex = map[y, x];
+
+
+
+
+                    if (textureIndex == 1)
+                    {
+
+                        Tile tile;
+                        Vector2 pos = new Vector2(48 * x, 48 * y);
+
+                        tile = new Tile(crate, 48, 48, pos, true);
+
+                        tileList.Add(tile);
+
+                        Console.WriteLine("x=" + x + " y=" + y + " is " + textureIndex);
+                    }
+                    else
+                    {
+                        Console.WriteLine("x=" + x + " y=" + y + " is " + textureIndex);
+                    }
+
+
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            playerVelocity.X = Clamp(playerVelocity.X, -400f, 400f);
+            
+
+            IsMouseVisible = true;
+            MouseState mouse = Mouse.GetState();
+            Console.WriteLine(mouse.X + " " + mouse.Y);
+
+            playerRect = new Rectangle((int)player.Position.X, (int)player.Position.Y, 24, 42);
+
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            
+
+            
+
+            KeyboardState keyboardState = Keyboard.GetState();
+            
+
+            foreach (Tile tile in tileList)
+            {
+                if (tile.BoundingBox.Intersects(playerRect))
+                {
+
+
+                    if (!standing)
+                        playerVelocity.Y = 0f;
+
+                    standing = true;
+
+                    
+                    break;
+                }
+                else
+                {
+                    //  playerVelocity.Y = 9.82f;
+                    standing = false;
+                    
+                }
+
+            }
+
+            if (!standing)
+            {
+                playerVelocity += new Vector2(0, 9.82f);
+            }
+            else
+            {
+                if (playerVelocity.X < 0)
+                {
+                    playerVelocity.X += 5.1f;
+                }
+                else if (playerVelocity.X > 0)
+                {
+                    playerVelocity.X -= 5.1f;
+                }
+
+                if (keyboardState.IsKeyDown(Keys.Up) && !prevKey.IsKeyDown(Keys.Up))
+                {
+                    standing = false;
+                    playerVelocity -= new Vector2(0, 350f);
+                }
+
+
+            }
+
+
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                playerVelocity -= new Vector2(10f, 0);
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                playerVelocity += new Vector2(10f, 0);
+            }
+
+          
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+
+                rotation += 0.1f;
+            }
+
+          
+
+       
+
+         
+
+                player.Position += playerVelocity * elapsed;
+
+                player.Update(gameTime);
+
+                prevKey = keyboardState;
+                
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            foreach (Tile tile in tileList)
+            {
+                tile.Draw(spriteBatch);
+                
+            }
+
+            player.Draw(spriteBatch, rotation);
+           // Console.WriteLine(player.Position.X + " " + player.Position.Y);
+            Console.WriteLine(standing);
+
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+
+
+
+
+        public float Clamp(float var, float min, float max)
+        {
+
+            if (var < min)
+                var = min;
+
+            if (var > max)
+                var = max;
+
+            return var;
+        
+        }
+
+
+    }
+}
---
+++ Plattformer/tile'r/tile'r/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace TestGame.TileEngine
+{
+    class Tile
+    {
+        public bool Landable { get; set; }
+        public int SpriteWidth { get; set; }
+        public int SpriteHeight { get; set; }
+        public Vector2 Position { get; set; }
+        public Texture2D Texture { get; set; }
+
+        public Rectangle BoundingBox
+        {
+            get { return new Rectangle((int)this.Position.X, (int)this.Position.Y, this.SpriteWidth, this.SpriteHeight); }
+        }
+
+        /*
+        public Tile()
+        {
+            this.SpriteHeight = 32;
+            this.SpriteWidth = 32;
+        }
+         */
+
+        public Tile(Texture2D texture, int spriteWidth, int spriteHeight, Vector2 startingPosition, bool isLandable)
+        {
+            this.Texture = texture;
+            this.SpriteWidth = spriteWidth;
+            this.SpriteHeight = spriteHeight;
+            this.Position = startingPosition;
+            this.Landable = isLandable;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            Vector2 scale = new Vector2(SpriteWidth / (float)this.Texture.Width, SpriteHeight / (float)this.Texture.Height);
+            sb.Draw(this.Texture, this.Position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 1f);
+        }
+    }
+}
