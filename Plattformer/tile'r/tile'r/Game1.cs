3f962bf1aeaefa599af6753d0ddfd537c013b7cf
Marin Jelica <Xe3d@live.se>
2013-03-03 16:15:04 +0100

    refactor

--- Plattformer/tile'r/tile'r/Game1.cs
+++ Plattformer/tile'r/tile'r/Game1.cs
@@ -56,7 +56,7 @@ namespace tile_r
             };
 
 
-        
+
 
         public Game1()
         {
@@ -92,24 +92,24 @@ namespace tile_r
 
             cam.Pos = new Vector2(682.0f, 334.0f);
 
-            
+
 
             Createflor();
 
             graphics.PreferredBackBufferWidth = 1280;
             graphics.PreferredBackBufferHeight = 720;
             graphics.ApplyChanges();
-            
-            
+
+
 
             sprite = Content.Load<Texture2D>("jasperrun");
             animation.Initialize(sprite, new Vector2(0, 0), 24, 42, 4, 150, Color.White, 1f, true);
-            
+
 
             player.Initialize(animation, new Vector2(10, 700));
 
             KeyboardState prevKey = Keyboard.GetState();
-            
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -171,29 +171,29 @@ namespace tile_r
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            
-            
+
+
             KeyboardState keyboardState = Keyboard.GetState();
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keyboardState.IsKeyDown(Keys.Escape))
                 this.Exit();
 
             playerVelocity.X = Clamp(playerVelocity.X, -400f, 400f);
-            
+
 
             IsMouseVisible = true;
             MouseState mouse = Mouse.GetState();
-          //  Console.WriteLine(mouse.X + " " + mouse.Y);
+            //  Console.WriteLine(mouse.X + " " + mouse.Y);
 
             playerRect = new Rectangle((int)player.Position.X, (int)player.Position.Y, 24, 42);
 
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            
 
-            
 
-            
+
+
+
 
             if (keyboardState.IsKeyDown(Keys.F) && !prevKey.IsKeyDown(Keys.F))
             {
@@ -213,8 +213,8 @@ namespace tile_r
 
             }
 
-            
-            
+
+
 
             foreach (Tile tile in tileList)
             {
@@ -240,18 +240,18 @@ namespace tile_r
                         if ((player.Position.Y + (player.Height / 2) - 5) >= (tile.Position.Y - (tile.SpriteHeight / 2)))
                             player.Position.Y = (tile.Position.Y - (tile.SpriteHeight / 2) - (player.Height / 2) + 5);
 
-                        
-                    }
-                    
 
-                    
+                    }
+
+
+
                     break;
                 }
                 else
                 {
                     //  playerVelocity.Y = 9.82f;
                     standing = false;
-                    
+
                 }
 
             }
@@ -269,7 +269,7 @@ namespace tile_r
                 if (!running)
                 {
                     playerVelocity.X /= 1.1f;
-                    
+
                     if (playerVelocity.X < 0)
                     {
                         playerVelocity.X += 2;
@@ -315,8 +315,8 @@ namespace tile_r
                 cam.Pos += new Vector2(0f, -2f);
             }
 
-            if (keyboardState.IsKeyUp(Keys.Left)&&keyboardState.IsKeyUp(Keys.Right))
-            running = false;
+            if (keyboardState.IsKeyUp(Keys.Left) && keyboardState.IsKeyUp(Keys.Right))
+                running = false;
 
             if (keyboardState.IsKeyDown(Keys.Left))
             {
@@ -334,7 +334,7 @@ namespace tile_r
                 running = true;
             }
 
-          
+
             if (keyboardState.IsKeyDown(Keys.Down))
             {
 
@@ -342,23 +342,23 @@ namespace tile_r
             }
 
 
-            
-
-            animation.frameTime = 130f - Math.Abs(playerVelocity.X/10);
-         
-
-                player.Position += playerVelocity * elapsed;
-
-                player.Update(gameTime);
-
-                prevKey = keyboardState;
 
 
-                if (Math.Abs(playerVelocity.X) > 55)
-                    animation.draw = true;
-                else
-                    animation.draw = false;
-                   
+            animation.frameTime = 130f - Math.Abs(playerVelocity.X / 10);
+
+
+            player.Position += playerVelocity * elapsed;
+
+            player.Update(gameTime);
+
+            prevKey = keyboardState;
+
+
+            if (Math.Abs(playerVelocity.X) > 55)
+                animation.draw = true;
+            else
+                animation.draw = false;
+
 
             // TODO: Add your update logic here
 
@@ -384,11 +384,11 @@ namespace tile_r
             foreach (Tile tile in tileList)
             {
                 tile.Draw(spriteBatch);
-                
+
             }
 
             player.Draw(spriteBatch, rotation);
-          //  Console.WriteLine(cam.Pos.X + " " + cam.Pos.Y);
+            //  Console.WriteLine(cam.Pos.X + " " + cam.Pos.Y);
             Console.WriteLine(playerVelocity.X);
 
             spriteBatch.End();
@@ -412,7 +412,7 @@ namespace tile_r
                 var = max;
 
             return var;
-        
+
         }
 
 
